ebeccd5d45da4e86a225e3473aa85c128687c887
wser <w.web.dev@gmail.com>
2023-08-11 14:55:27 +0200

    bump vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,16 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), splitVendorChunkPlugin()],
-  // build: {
-  //   rollupOptions: {
-  //     output: {
-  //       manualChunks(id: any) {
-  //         if (id.includes("node_modules")) {
-  //           return id.split("/node_modules/").pop()?.split("/")[0]; // or simply return 'vendor' for a single vendor chunk
-  //         }
-  //       }
-  //     }
-  //   }
-  // },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id: any) {
+          if (id.includes("node_modules")) {
+            return id.split("/node_modules/").pop()?.split("/")[0]; // or simply return 'vendor' for a single vendor chunk
+          }
+        }
+      }
+    }
+  },
 })
 
